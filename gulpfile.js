894813f5aed08bd82ee7959e0ae92c66227f041a
Erick Sarmiento <mychip13@hotmail.com>
2017-08-26 14:14:55 -0500

    instalacion de gulp-rename y configuracion del gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
+gulp.task('styles', function () {
+	gulp
+		.src('index.scss')
+		.pipe(sass())
+		.pipe(rename('app.css'))
+		.pipe(gulp.dest('public'));
+})
+
+gulp.task('default', ['styles'])
