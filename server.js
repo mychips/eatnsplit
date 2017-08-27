b88a8214440f4489837caeb303aac2798dc5e974
Erick Sarmiento <mychip13@hotmail.com>
2017-08-27 09:26:44 -0500

    se instalo yo-yo y se configuro la home  y signup

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/signin', function (req, res) {
 	res.render('index');
 })
 
-app.listen(7000, function (err) {
+app.listen(13000, function (err) {
 	if (err) return console.log('Hubo un error'), process.exit(1);
-	console.log('Escuchando en el puerto 7000');
+	console.log('Escuchando en el puerto 13000');
 })
