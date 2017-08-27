332a5cf1ccb227e0fe076add9c9d6ae66d40213a
Erick Sarmiento <mychip13@hotmail.com>
2017-08-27 08:40:40 -0500

    Instalacion de watchify

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get('/', function (req, res) {
 	res.render('index');
 })
 
+app.get('/signup', function (req, res) {
+	res.render('index');
+})
+
+app.get('/signin', function (req, res) {
+	res.render('index');
+})
+
 app.listen(7000, function (err) {
 	if (err) return console.log('Hubo un error'), process.exit(1);
 	console.log('Escuchando en el puerto 7000');
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert('hola Erick');
+var page = require('page');
+
+var main = document.getElementById('main-container');
+
+page('/', function (ctx, next) {
+	main.innerHTML = 'Home <a href="/signup">Signup</a>';
+})
+
+page('/signup', function (ctx, next) {
+	main.innerHTML = 'Signup <a href="/">Home</a>';
+})
+
+page();
