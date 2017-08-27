a00af3cf863a4b3e5edbcceeb11823602db5adba
Erick Sarmiento <mychip13@hotmail.com>
2017-08-27 09:03:32 -0500

    se definieron tareas para correr el server y escuchar cambios

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var rename = require('gulp-rename');
 var babel = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 gulp.task('styles', function () {
 	gulp
@@ -19,13 +20,31 @@ gulp.task('assets', function () {
 		.pipe(gulp.dest('public'));
 })
 
-gulp.task('scripts', function () {
-	browserify('./src/index.js')
-		.transform(babel)
-		.bundle()
-		.pipe(source('index.js'))
-		.pipe(rename('app.js'))
-		.pipe(gulp.dest('public'));
-})
+function compile(watch) {
+	var bundle = watchify(browserify('./src/index.js'))
 
-gulp.task('default', ['styles', 'assets', 'scripts']);
+	function rebundle() {
+		bundle
+			.transform(babel)
+			.bundle()
+			.pipe(source('index.js'))
+			.pipe(rename('app.js'))
+			.pipe(gulp.dest('public'));
+	}
+	if (watch) {
+		bundle.on('update', function () {
+			console.log('---> Bundling...');
+			rebundle();
+		})
+	}
+
+	rebundle();
+}
+
+gulp.task('build', function () {
+	return compile();
+});
+
+gulp.task('watch', function () { return compile(true); });
+
+gulp.task('default', ['styles', 'assets', 'build']);
