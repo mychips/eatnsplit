a32c8e3dd3328a503dbaf895d7c5e84823c9fe33
Erick Sarmiento <mychip13@hotmail.com>
2017-08-26 21:18:56 -0500

    Instalacion de browserify, babelify y vinyl-source

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('styles', function () {
 	gulp
@@ -16,4 +19,13 @@ gulp.task('assets', function () {
 		.pipe(gulp.dest('public'));
 })
 
-gulp.task('default', ['styles', 'assets'])
+gulp.task('scripts', function () {
+	browserify('./src/index.js')
+		.transform(babel)
+		.bundle()
+		.pipe(source('index.js'))
+		.pipe(rename('app.js'))
+		.pipe(gulp.dest('public'));
+})
+
+gulp.task('default', ['styles', 'assets', 'scripts']);
